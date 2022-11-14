3893f9967e9efc9d3eeb593f822a34954dce8f92
Ryan Paglione <ryanpaglionework@gmail.com>
2022-11-14 11:32:59 -0600

    Frontend Tooling

--- frontend/cypress/e2e/slidertest.cy.ts
+++ frontend/cypress/e2e/slidertest.cy.ts
@@ -1,5 +1,11 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+describe('testing toggle sliders', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3001')
   })
+
+  it('checks slide', () => {
+    cy.get('h1')
+  })
+
 })
